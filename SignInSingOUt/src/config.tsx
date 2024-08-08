5c013e7472ced6424b4c7e1805048154fc5ddcdd
JohnReyesBehaven <jreyes@behavenkids.com>
2024-08-07 23:20:39 -0500

    Update config.tsx

--- SignInSingOUt/src/config.tsx
+++ SignInSingOUt/src/config.tsx
@@ -1,8 +1,8 @@
-const backEndWebApi = 'http://192.168.0.9:7012/';
-//const backEndWebApi = 'http://localhost:5025/';
+//const backEndWebApi = 'http://192.168.0.9:7012/';
+const backEndWebApi = 'http://localhost:5025/';
 
-//const frontEndWebApi = 'http://localhost:5173/';
-const frontEndWebApi = 'http://daycarelogistics.behavndaycare.local/';
+const frontEndWebApi = 'http://localhost:5173/';
+//const frontEndWebApi = 'http://daycarelogistics.behavndaycare.local/';
 
 
 
